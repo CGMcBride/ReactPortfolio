7e56625cca9222e3f77ab64ac40b18402d4ba0f9
CGMcBride <connermcbride@ymail.com>
2020-07-25 18:04:10 -0500

    added contact form for emailer

--- myportfolio/src/components/Contact/index.js
+++ myportfolio/src/components/Contact/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Infotitle from '../Infotitle';
+import Form from './Form';
 
 export default function index() {
     return (
@@ -13,26 +14,8 @@ export default function index() {
             <div className="jumbotron jumbotron-fluid">
                 <div className="container">
                     <div className="col-12">
-                        <form action="https://formspree.io/FORM_ID" method="POST">
-                            <div className="form-group">
-                                <label className="text-info" for="exampleFormControlInput1">Your Name:</label>
-                                <input type="text" name="name" className="form-control" id="exampleFormControlInput1"
-                                    placeholder="Type your name here" />
-                            </div>
+                        <Form />
 
-                            <div className="form-group">
-                                <label className="text-info" for="exampleFormControlInput2">Your Email:</label>
-                                <input type="email" name="email" className="form-control" id="exampleFormControlInput2"
-                                    placeholder="Type your email here" />
-                            </div>
-
-                            <div className="form-group">
-                                <label className="text-info" for="exampleFormControlTextarea1">Your Message: </label>
-                                <textarea className="form-control" id="exampleFormControlTextarea1" name="message"
-                                    rows="2"></textarea>
-                            </div>
-                            <input className="btn btn-danger mt-3" type="submit" value="Send" />
-                        </form>
                     </div>
 
                 </div>
