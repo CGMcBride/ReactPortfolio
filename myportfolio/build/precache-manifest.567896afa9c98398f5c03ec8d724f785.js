87d369b45c1c8bd238e266669c93cfb38b5b2c2d
CGMcBride <connermcbride@ymail.com>
2020-08-12 17:52:52 -0500

    added a build for heroku connection

---
+++ myportfolio/build/precache-manifest.567896afa9c98398f5c03ec8d724f785.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a63f8a310a9056e4db638c627fd51f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32324d3258a5e1ff0ce4",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "499cc007bb862237e9df",
+    "url": "/static/css/main.6a602afe.chunk.css"
+  },
+  {
+    "revision": "32324d3258a5e1ff0ce4",
+    "url": "/static/js/2.07954509.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.07954509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "499cc007bb862237e9df",
+    "url": "/static/js/main.b23f7583.chunk.js"
+  },
+  {
+    "revision": "e3473a344493b6ab086d",
+    "url": "/static/js/runtime-main.21e4bbbf.js"
+  }
+]);
