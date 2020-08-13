f7a2be5beaf7be57e85bc02e3291abb768a3e4dd
CGMcBride <connermcbride@ymail.com>
2020-08-12 19:07:57 -0500

    adding app.get

--- server.js
+++ server.js
@@ -7,4 +7,11 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("client/build"));
-app.listen(PORT)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/'));
+});
+
+app.listen(PORT, () => {
+    console.log('Port is running');
+});
